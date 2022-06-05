9fd12ca43588b208cc7c37ecccc5f8d5f4097266
chromecodes <hameedhussain782@gmail.com>
2022-06-05 23:03:14 +0530

    UPDATED the input function and styled inputs

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
 slider = document.getElementById('myRange');
 output = document.getElementById('sliderValue');
-rainbow = document.getElementById('rainbow')
-classic = document.getElementById('classic')
-eraser = document.getElementById('eraser')
-clear = document.getElementById('clear')
-pen = document.getElementById('pen')
-canvas = document.getElementById('canvas')
+rainbow = document.getElementById('rainbow');
+classic = document.getElementById('classic');
+eraser = document.getElementById('eraser');
+clear = document.getElementById('clear');
+pen = document.getElementById('pen');
+canvas = document.getElementById('canvas');
+//gray = document.getElementById('grayscale');
 
 let v = slider.value;
 a = v*v;
 c = 0;
-active = ""
+active = "";
 penColour = pen.value;
-bg = canvas.value
-console.log(bg);
-console.log(canvas.value);
+bg = canvas.value;
 output.innerHTML = slider.value+' x '+slider.value;
-createDiv(a,v)
+createDiv(a,v);
 
 pen.addEventListener('click', function(e){
     pen.addEventListener("change", function(e){
-        classic.style.cssText = ''
-        rainbow.style.cssText = ''
-        eraser.style.cssText = ""
+        classic.style.cssText = '';
+        rainbow.style.cssText = '';
+        eraser.style.cssText = "";
         penColour = e.target.value;
-        active = e.target.id
+        active = e.target.id;
         listen(e);
     });
 });
 canvas.addEventListener('click', function(e){
     canvas.addEventListener("change", function(e){
-        bg = e.target.value
-        active = e.target.id
+        bg = e.target.value;
+        active = e.target.id;
         grids = document.querySelectorAll ('.cell');
             grids.forEach((grid) => {grid.style.cssText = ' background-color:'+bg+';';
             });
@@ -42,30 +41,44 @@ canvas.addEventListener('click', function(e){
     
 
 classic.addEventListener('click', function(e){
-    classic.style.cssText = ' color: rgb(230, 230, 230); background-color: rgb(0, 0, 0);'
-    rainbow.style.cssText = ''
-    eraser.style.cssText = ""
-    penColour = "black"
-    active = e.target.id
+    classic.style.cssText = 'color: var(--primary-light); background-color: var(--primary-dark);';
+    rainbow.style.cssText = '';
+  //  gray.style.cssText = "";
+    eraser.style.cssText = "";
+    penColour = "black";
+    active = e.target.id;
     listen();
 });
 rainbow.addEventListener('click', function(e){
-    classic.style.cssText = ' '
-    rainbow.style.cssText = 'color: rgb(230, 230, 230); background-color: rgb(0, 0, 0);'
-    eraser.style.cssText = ' '
-    active = e.target.id
+    classic.style.cssText = '';
+    rainbow.style.cssText = 'color: var(--primary-light); background-color: var(--primary-dark);';
+   // gray.style.cssText = "";
+    eraser.style.cssText = '';
+    active = e.target.id;
+    listen();
+});
+/*
+gray.addEventListener('click', function(e){
+    classic.style.cssText = '';
+    rainbow.style.cssText = '';
+    gray.style.cssText = 'color: var(--primary-light); background-color: var(--primary-dark);';
+    eraser.style.cssText = '';
+    penColour ='background:rgba(0, 0, 0, 0.1);'
+    active = e.target.id;
+    listen();
+});
+*/
+eraser.addEventListener('click', function(e){
+    classic.style.cssText = '';
+    rainbow.style.cssText = '';
+//gray.style.cssText = "";
+    eraser.style.cssText = 'color: var(--primary-light); background-color: var(--primary-dark);';
+    penColour = "aliceblue";
+    console.log(penColour);
+    active = e.target.id;
     listen();
 });
 
-eraser.addEventListener('click', function(e){
-    classic.style.cssText = ' '
-    rainbow.style.cssText = ''
-    eraser.style.cssText = ' color: rgb(230, 230, 230); background-color: rgb(0, 0, 0);'
-    penColour = "aliceblue"
-    console.log(penColour);
-    active = e.target.id
-    listen();
-});
 clear.addEventListener('click', function(){
     grids = document.querySelectorAll ('.cell');
     grids.forEach((grid) => {grid.style.cssText = 'transition: background-color 1s ease-in-out; background-color: '+bg+';';
@@ -74,7 +87,7 @@ clear.addEventListener('click', function(){
 
 
 function multiColour(){
-    rainBow = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red']
+    rainBow = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
     if( c == 7 ){
         c = 0;
     };
@@ -92,13 +105,13 @@ slider.oninput = function(){
     v = this.value;
     a = v**2;
     output.innerHTML = this.value+' x '+this.value;
-    createDiv(a,v)
+    createDiv(a,v);
 }
 
 function clearDiv(a){
     for ( let  i = 1; i <= a; i++){
-    let div = container.querySelector('div')
-    container.removeChild(div)
+    let div = container.querySelector('div');
+    container.removeChild(div);
     }
 }
 
@@ -106,39 +119,82 @@ function createDiv(a,v){
     for ( let  i = 1; i <= a; i++){
         const div = document.createElement ('div');
         div.classList.add(`cell`);
-        div.classList.add(`${i}`);
         container.appendChild(div);  
-        container.style.cssText = `grid-template-columns: repeat(${v}, 1fr)`
+        container.style.cssText = `grid-template-columns: repeat(${v}, 1fr)`;
     }
-   listen()
+   listen();
 }
 
 function listen(){
+    console.log(active);
     if(active == "rainbow"){
         grids = document.querySelectorAll ('.cell');
         grids.forEach((grid) => {grid.addEventListener('mouseenter', function(e){
-            e.target.style.cssText = 'background-color: '+ multiColour()+';';
+            this.style.cssText = 'background-color: '+ multiColour()+';';
             });
         });
-} else {
+    } 
+    /*
+    else if (active == "grayscale") {
     grids = document.querySelectorAll ('.cell');
     grids.forEach((grid) => {grid.addEventListener('mouseenter', function(e){
-        e.target.style.cssText = 'background-color: '+ penColour+';';
+        if (e.target.classList[1] == 'grey'){
+            console.log(0);
+            console.log(e.target.style.backgroundColor);
+            if(e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.1)'){
+                console.log(1);
+                console.log(e.target.style[9]);
+
+                e.target.style.cssText ='background:rgba(0, 0, 0, 0.3);'
+
+            } else if (e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.4)'){
+                e.target.style.cssText = 'background: rgb(0, 0, 0, 0.6);';
+                console.log(e.target.style.backgroundColor);
+                console.log(2);
+            } else if (e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.6)'){
+                e.target.style.cssText = 'background: rgb(0, 0, 0, 0.8);';
+                console.log(e.target.style.backgroundColor);
+                console.log(3);
+            } else {
+                e.target.style.cssText ='background:rgba(0, 0, 0, 1);';
+                console.log(e.target.style);
+                console.log(4);
+            }
+        } 
+        else {
+            e.target.classList.add('grey');
+            e.target.style.cssText ='background:rgba(0, 0, 0, 0.1);';
+        }
+        });
     });
-    });
+    } 
+    */
+    else {
+        grids = document.querySelectorAll ('.cell');
+        grids.forEach((grid) => {grid.addEventListener('mouseenter', function(e){
+            this.style.cssText = 'background-color: '+penColour+';';
+        });
+        });
+    }
 }
-}
-
-
-function run(e){
-   e.target.style.cssText = 'background-color: black;'
-}
-
 
 slider.addEventListener('mousemove', function(){
     let l = slider.value*1.5 ;
-    let color = 'linear-gradient(90deg, rgb(0,0,0)'+l+'%, rgb(211, 211, 211)'+l+'%)'
+    let color = 'linear-gradient(90deg, rgb(0,0,0)'+l+'%, rgb(211, 211, 211)'+l+'%)';
     slider.style.background = color;
 })
 
-
+/*
+if(e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.1)'){
+    e.target.style.cssText = 'background: rgb(0, 0, 0, 0.4);';
+    console.log(e.target.style.backgroundColor);
+} else if (e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.4)'){
+    e.target.style.cssText = 'background: rgb(0, 0, 0, 0.6);';
+    console.log(e.target.style.backgroundColor);
+} else if (e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.6)'){
+    e.target.style.cssText = 'background: rgb(0, 0, 0, 0.8);';
+    console.log(e.target.style.backgroundColor);
+} else if (e.target.style.backgroundColor == 'rgba(0, 0, 0, 0.8)'){
+    e.target.style.cssText = 'background: rgb(0, 0, 0, 1);';
+    console.log(e.target.style.backgroundColor);
+};*/
